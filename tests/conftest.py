14a067af8faf06871c2fba4dce3b71aa8c6b3d78
Nikita Vikulin <nikitavik89@gmail.com>
2023-03-08 11:49:22 +0300

    Добавлены шаги теста

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ from selene import browser
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser():
     browser.config.hold_browser_open = True
-    browser.config.window_width = '1024'
-    browser.config.window_height = '768'
+    browser.config.window_width = '1980'
+    browser.config.window_height = '1024'
     browser.config.timeout = 6.0
 
     yield
--- tests/test_1.py
+++ tests/test_1.py
@@ -2,7 +2,26 @@ from selene import browser, have, be
 
 
 def test_ui_form():
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.element('#firstName').type('Ivan')
+    browser.element('#lastName').type('Ivanov')
+    browser.element('#userEmail').type('ivanov@mail.ru')
+    browser.element('//*[@id="genterWrapper"]/div[2]/div[1]/label').click()
+    # browser.element('//*[@id="subjectsContainer"]/div/div[1]').type('1234567891')
+    # browser.element('#dateOfBirthInput').click().type('')
+    browser.element('//*[@id="subjectsContainer"]/div').type('Math')
+    browser.element('//*[@id="hobbiesWrapper"]/div[2]/div[1]/label').click()
+
+    browser.element('#currentAddress').type('Moscow')
+    browser.element('//*[@id="state"]/div/div[1]/div[1]').click()
+    browser.element('//*[@id="react-select-3-option-1"]').click()
+    browser.element('//*[@id="city"]/div/div[1]').click()
+    browser.element('//*[@id="react-select-4-option-1"]').click()
 
 
-    browser.open('https://todomvc.com/examples/emberjs/')
-    browser.element('#new-todo').type('a').press_enter()
+    # browser.element('#submit').click()
+
+
+
+
+    # browser.element('#firstName').type('Ivan').press_enter()
